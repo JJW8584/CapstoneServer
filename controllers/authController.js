f832a6a5ed1fbe76bc9ca6c7c07a007cf81c3269
JJW <jjw8584@gmail.com>
2025-06-12 17:21:23 +0900

    파라미터 수정

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,7 @@
 const userService = require('../services/userService');
 
 exports.login = async (req, res) => {
-    const { email, password } = req.body;
+    const { userId, password } = req.body;
     console.log('로그인 API 진입');
     console.log('로그인 정보 받음', req.body);
 
@@ -13,6 +13,7 @@ exports.login = async (req, res) => {
 
     try {
         // 1. 디비 서버에 로그인 요청 보내기
+        const email = userId;
         const result = await userService.loginToDatabaseServer(email, password);
 
         // 2. 디비 서버가 응답했는지 확인
