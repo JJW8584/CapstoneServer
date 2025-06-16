2b1fa4db7af4e580d753a07bc98c0101edd4f4a7
JJW <jjw8584@gmail.com>
2025-06-16 17:02:07 +0900

    그룹 라우터 수정

--- routes/group.js
+++ routes/group.js
@@ -8,9 +8,6 @@ router.post('/profile', groupController.profile);
 // 사용자가 속한 그룹 정보 호출 api/group/groups
 router.post('/groups', groupController.groups);
 
-// 그룹 참여 요청 api/group/join
-router.post('/join', groupController.join);
-
 // 그룹 생성 요청 api/group/createGroup
 router.post('/createGroup', groupController.createGroup);
 
